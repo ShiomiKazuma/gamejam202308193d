bbfbdf413b0d1e16c5391f81717b1ea42365813b
tanakasora1203 <sora1042hym@gmail.com>
2023-08-20 13:52:56 +0900

    ゴール処理の修正（音の追加）

--- Assets/TanakaSora/Goal.cs
+++ Assets/TanakaSora/Goal.cs
@@ -6,12 +6,19 @@ using UnityEngine.SceneManagement;
 public class Goal : MonoBehaviour
 {
     [SerializeField] SceneObject _sceneObject = default;
-    [SerializeField]ScoreManager _scoreManager;
+    [SerializeField] ScoreManager _scoreManager;
+    [SerializeField] AudioClip _sound1;
+    AudioSource _audioSource;
 
-  private void OnCollisionEnter(Collision collision)
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+    private void OnCollisionEnter(Collision collision)
   {
         if(collision.gameObject.tag == "Player")
         {
+            _audioSource.PlayOneShot(_sound1);
             _scoreManager.BestScore();
             SceneManager.LoadScene(_sceneObject);
         }
