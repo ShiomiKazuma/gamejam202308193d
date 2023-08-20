f7c693e4e6b27f2f3cb84c8546c6fe6c6b5ed8b1
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-08-20 11:15:03 +0900

    [add]スコアマネジャーの追加

---
+++ Assets/Shiomi/Script/ScoreManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    float _bestScore;
+    [SerializeField] TimeManager _timeManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool exist = PlayerPrefs.HasKey("Score");
+
+        if (exist)
+        {
+            // ’l‚ª•Û‘¶‚³‚ê‚Ä‚¢‚é‚Æ‚«‚Ìˆ—
+            _bestScore = PlayerPrefs.GetFloat("Score");
+        }
+        else
+        {
+            // ’l‚ª•Û‘¶‚³‚ê‚Ä‚¢‚È‚¢‚Æ‚«‚Ìˆ—
+            PlayerPrefs.SetFloat("Score", 0f);
+            _bestScore = PlayerPrefs.GetFloat("Score");
+        }
+    }
+
+    public void BestScore()
+    {
+        if(_bestScore < _timeManager._timer)
+        {
+            PlayerPrefs.SetFloat("Score", _timeManager._timer);
+        }
+    }
+}
